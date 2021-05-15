8b98857637576756b58ecafd070720845ec6e143
m <mkostin325@gmail.com>
2021-05-15 11:32:04 +0300

    Finale

--- Text/Text/main.cpp
+++ Text/Text/main.cpp
@@ -9,34 +9,93 @@ void main()
 	int exit = -1;
 	text.Read("manuscript.txt");
 	text.Print();
-
+	text.GoFirstLink();
 	while (exit != 0)
 	{
-		cout << "Print 0 to stop" << endl;
-		cout << "Print 1 to read text from manuscript.txt" << endl;
-		cout << "Print 2 to write text and save it in origin.txt" << endl;
+		cout << "Print 0 to save changes in origin.txt and close the programm" << endl;
+		cout << "Print 1 to go next line" << endl;
+		cout << "Print 2 to go down line" << endl;
+		cout << "Print 3 to insert next line" << endl;
+		cout << "Print 4 to insert down line" << endl;
+		cout << "Print 5 to insert next section" << endl;
+		cout << "Print 6 to insert down section" << endl;
+		cout << "Print 7 to delete next line" << endl;
+		cout << "Print 8 to delete down line" << endl;
+		cout << "Print 9 to clean" << endl;
+		cout << "Print 10 to go first line" << endl;
+		cout << "Print 11 to print text" << endl;
 		cin >> exit;
 		if (exit == 0)
+		{
+			text.Save("origin.txt");
 			break;
+		}
 		if (exit == 1)
 		{
-			TTextLink::InitMem(100);
-			TText text;
-			text.Read("manuscript.txt");
-			text.Print();
+			text.GoNextLink();
 		}
 		if (exit == 2)
 		{
-			TText text;
-			string buff;
-			char str[] = "Try this";
-			cin >> buff;
-			//cout << buff << endl;
-			//cout << str << endl;
-			//text.InsNextLine(str);
-			//text.Print();
-			//text.Save("origin.txt");
+			text.GoDownLink();
 		}
+		if (exit == 3)
+		{
+			char line[80];
+			cin >> line;
+			text.InsNextLine(line);
+			text.Print();
+			text.GoFirstLink();
+		}
+		if (exit == 4)
+		{
+			char line[80];
+			cin >> line;
+			text.InsDownLine(line);
+			text.Print();
+			text.GoFirstLink();
+		}
+		if (exit == 5)
+		{
+			char line[80];
+			cin >> line;
+			text.InsNextSection(line);
+			text.Print();
+			text.GoFirstLink();
+		}
+		if (exit == 6)
+		{
+			char line[80];
+			cin >> line;
+			text.InsDownSection(line);
+			text.Print();
+			text.GoFirstLink();
+		}
+		if (exit == 7)
+		{
+			text.DelNextLine();
+			text.Print();
+			text.GoFirstLink();
+		}
+		if (exit == 8)
+		{
+			text.DelDownLine();
+			text.Print();
+			text.GoFirstLink();
+		}
+		if (exit == 9)
+		{
+			TTextLink::Clean(text);
+		}
+		if (exit == 10)
+		{
+			text.GoFirstLink();
+		}
+		if (exit == 11)
+		{
+			text.Print();
+			text.GoFirstLink();
+		}
+
 	}
 
 	//TTextLink::PrintFree();
--- Text/Text/text.h
+++ Text/Text/text.h
@@ -1,13 +1,11 @@
 #pragma once
-#include <stdlib.h>
-#include <string>
-#include "tstack.h"
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include<stdio.h>
+#include "tstack.h"
 
-class TTextLink;
 class TText;
-
+class TTextLink;
 struct TMem
 {
 	TTextLink* pFirst, * pLast, * pFree;
@@ -16,54 +14,38 @@ struct TMem
 class TTextLink
 {
 public:
+	bool flag;
 	TTextLink* pNext, * pDown;
 	char str[80];
-	bool flag;
 	static TMem mem;
-	TTextLink(char* s = NULL, TTextLink* pn = NULL, TTextLink* pd = NULL)
-	{
-		pNext = pn;
-		pDown = pd;
-		if (!s)
-			str[0] = '\n';
-		else
-			strcpy_s(str, s);
-	}
-
+	TTextLink(char* s = NULL, TTextLink* pn = NULL, TTextLink* ph = NULL);
+	void* operator new (std::size_t n);
+	void operator delete (void* p);
 	static void InitMem(int size = 100);
-
-	static void PrintFree(TText& t);
-
+	static void PrintFree();
 	static void Clean(TText& t);
-
-	void* operator new(size_t n)
-	{
-		TTextLink* pC = mem.pFree;
-		if (mem.pFree != NULL)
-			mem.pFree = pC->pNext;
-		return pC;
-	}
-
-	void operator delete(void* p)
-	{
-		TTextLink* pC = (TTextLink*)p;
-		pC->pNext = mem.pFree;
-		mem.pFree = pC;
-	}
 };
+
+
 
 class TText
 {
-	TTextLink* pFirst, * pCurr;
-	TStack<TTextLink*> st;
 	int level;
+	TTextLink* pFirst;
+	TTextLink* pCurr;
+	TStack<TTextLink*> st;
+	//static TMem mem;
 public:
+	void SetCurrFlagTrue()
+	{
+		pCurr->flag = true;
+	}
 	void GoFirstLink()
 	{
 		pCurr = pFirst;
-		st.Clear();
+		if (!st.Empty())
+			st.Clear();
 	}
-
 	void GoNextLink()
 	{
 		if (pCurr->pNext != NULL)
@@ -72,210 +54,103 @@ public:
 			pCurr = pCurr->pNext;
 		}
 	}
-
 	void GoDownLink()
 	{
-		if (!st.Empty())
-			pCurr = st.Pop();
+		if (pCurr->pDown != NULL)
+		{
+			st.Push(pCurr);
+			pCurr = pCurr->pDown;
+		}
 	}
-
 	void GoPrevLink()
 	{
-		if (pFirst != pCurr)
-			pCurr = st.Pop();
-	}
-
-	void InsNextLine(char* s)
-	{
-		TTextLink* t;
-		t = new TTextLink;
-		strcpy_s(t->str, s);
-		t->pNext = pCurr->pNext;
-		t->pDown = NULL;
-		pCurr->pNext = t;
-	}
-
-	void InsNextSection(char* s)
-	{
-		TTextLink* t;
-		t = new TTextLink(s, NULL, pCurr->pNext);
-		pCurr->pNext = t;
-	}
-
-	void InsDownSection(char* s)
-	{
-		TTextLink* t = new TTextLink(s, NULL, pCurr->pDown);
-		pCurr->pDown = t;
-	}
-
-	void InsDownLine(char* s)
-	{
-		TTextLink* t = (TTextLink*)(TTextLink::operator new(1));
-		strcpy_s(t->str, s);
-		t->pNext = pCurr->pDown;
-		t->pDown = NULL;
-		pCurr->pDown = t;
-	}
-
-	void DelNextLine()
-	{
-		if (pCurr->pNext)
+		if (!st.Empty())
 		{
-			TTextLink* t = pCurr->pNext;
-			pCurr->pNext = t->pNext;
-			delete t;
+			pCurr = st.Top();
+			st.Pop();
 		}
 	}
-
-	void DelDownLine()
-	{
-		if (pCurr->pDown)
-		{
-			TTextLink* t = pCurr->pDown;
-			pCurr->pDown = t->pNext;
-			delete t;
-		}
-	}
-
-	void Print()
-	{
-		level = 0;
-		PrintRec(pFirst);
-	}
-
-	void PrintRec(TTextLink* t)
-	{
-		if (t != NULL)
-		{
-			for (int i = 0; i < level; i++)
-				cout << " ";
-			cout << t->str << endl;
-			level++;
-			PrintRec(t->pDown);
-			level--;
-			PrintRec(t->pNext);
-		}
-	}
-
-	void Save(const char* fn)
-	{
-		ofstream osf;
-		osf.open(fn);
-		SaveRec(pFirst, osf);
-		osf.close();
-	}
-
-	void SaveRec(TTextLink* t, ofstream& osf)
-	{
-		if (t != NULL)
-		{
-			osf << t->str << endl;
-		}
-		if (t->pDown != NULL)
-		{
-			osf << '{' << endl;
-			SaveRec(t->pDown, osf);
-			osf << '}' << endl;
-		}
-		if (t->pNext != NULL)
-		{
-			SaveRec(t->pNext, osf);
-		}
-	}
-
-	void Read(const char* fn)
-	{
-		ifstream ifs(fn);
-		pFirst = ReadRec(ifs);
-		ifs.close();
-	}
-
-	TTextLink* ReadRec(istream& ifs)
-	{
-		TTextLink* pF = NULL, * pC = NULL;
-		char buff[80];
-		while (ifs.eof() == false)
-		{
-			ifs.getline(buff, 80, '\n');
-			if (buff[0] == '}')
-				break;
-			else
-				if (buff[0] == '{')
-				{
-					pC->pDown = ReadRec(ifs);
-				}
-				else
-				{
-					//TTextLink* tmp = new TTextLink(buff);
-					if (pC == NULL)
-					{
-						TTextLink* tmp = (TTextLink*)TTextLink::operator new(1);
-						strcpy_s(tmp->str, buff);
-						tmp->pDown = NULL;
-						tmp->pNext = NULL;
-						pF = pC = tmp;
-					}
-					else
-					{
-						TTextLink* tmp = (TTextLink*)TTextLink::operator new(1);
-						strcpy_s(tmp->str, buff);
-						tmp->pDown = NULL;
-						tmp->pNext = NULL;
-						pC->pNext = tmp;
-						pC = tmp;
-					}
-				}
-		}
-		return pF;
-	}
-
-	void Reset()
-	{
-		if (pFirst)
-		{
-			st.Clear();
-			pCurr = pFirst;
-			st.Push(pFirst);
-			if (pFirst->pNext)
-				st.Push(pFirst->pNext);
-			if (pFirst->pDown)
-				st.Push(pFirst->pDown);
-		}
-	}
-
-	void GoNext()
-	{
-		pCurr = st.Pop();
-		if (pCurr != pFirst)
-		{
-			if (pCurr->pNext)
-				st.Push(pCurr->pNext);
-			if (pCurr->pDown)
-				st.Push(pCurr->pDown);
-		}
-	}
-
-	bool IsEnd()
-	{
-		return st.Empty();
-	}
-
-	void SetCurrFlagTrue()
-	{
-		pCurr->flag = true;
-	}
+	void InsNextLine(char* s);
+	void InsNextSection(char* s);
+	void InsDownLine(char* s);
+	void InsDownSection(char* s);
+	void DelNextLine();
+	void DelDownLine();
+	void Print();
+	void PrintRec(TTextLink* t);
+	void Save(const char* fn);
+	void SaveRec(TTextLink* t, std::ofstream& osf);
+	void Read(const char* fn);
+	TTextLink* ReadRec(std::ifstream& ifs);
+	void Reset();
+	void GoNext();
+	bool IsEnd();
 };
+
+TTextLink::TTextLink(char* s, TTextLink* pn, TTextLink* ph)
+{
+	if (s == NULL)
+		str[0] = '\0';
+	else
+	{
+		int i = 0;
+		do
+		{
+			str[i] = s[i];
+			i++;
+		} while (s[i - 1] != '\0');
+	}
+	pNext = pn;
+	pDown = ph;
+}
+
+void* TTextLink::operator new (std::size_t n)
+{
+	TTextLink* pC = mem.pFree;
+	if (mem.pFree->pNext != NULL)
+	{
+		mem.pFree = mem.pFree->pNext;
+	}
+	return pC;
+}
+
+void TTextLink::operator delete (void* p)
+{
+	TTextLink* pC = (TTextLink*)p;
+	pC->pNext = mem.pFree;
+	mem.pFree = pC;
+}
+
+void TTextLink::InitMem(int size)
+{
+	int esize = sizeof(TTextLink) * size;
+	mem.pFirst = mem.pFree = (TTextLink*)malloc(esize);
+	mem.pLast = mem.pFirst + (size - 1);
+	TTextLink* tmp = mem.pFree;
+	while (tmp != mem.pLast)
+	{
+		tmp->pNext = tmp + 1;
+		tmp->flag = false;
+		tmp->str[0] = '\0';
+		tmp = tmp->pNext;
+	}
+	tmp->pNext = NULL;
+	tmp->flag = true;
+	tmp->str[0] = '\0';
+}
 
 void TTextLink::Clean(TText& t)
 {
 	TTextLink* tmp = mem.pFree;
-	while (tmp != NULL)
+	while (tmp != mem.pLast)
 	{
 		tmp->flag = true;
 		tmp = tmp->pNext;
 	}
+	tmp->flag = true;//?
 	for (t.Reset(); !t.IsEnd(); t.GoNext())
 	{
+		//t.pCurr->flag = 1;
 		t.SetCurrFlagTrue();
 	}
 	tmp = mem.pFirst;
@@ -285,27 +160,15 @@ void TTextLink::Clean(TText& t)
 			tmp->flag = false;
 		else
 			delete tmp;
+		tmp++;
 	}
-}
-
-void TTextLink::InitMem(int size)
-{
-	int Size = sizeof(TTextLink) * size;
-	mem.pFirst = mem.pFree = (TTextLink*)malloc(Size);
-	mem.pLast = mem.pFirst + (size - 1); //Size or size?
-	TTextLink * tmp = mem.pFirst;
-	while (tmp != mem.pLast)
-	{
-		tmp->pNext = tmp + 1;
+	if (tmp->flag)
 		tmp->flag = false;
-		tmp->str[0] = '\0';
-		tmp = tmp->pNext;
-	}
-	tmp->pNext = NULL;
-	tmp->flag = false;
+	else
+		delete tmp;
 }
 
-void TTextLink::PrintFree(TText& t)
+void TTextLink::PrintFree()
 {
 	TTextLink* tmp = mem.pFree;
 	while (tmp != mem.pLast)
@@ -316,4 +179,181 @@ void TTextLink::PrintFree(TText& t)
 	}
 	if (tmp->str[0] != '\0')
 		std::cout << tmp->str << ' ';
+	std::cout << std::endl;
+}
+
+void TText::InsNextLine(char* s)
+{
+	TTextLink* t = (TTextLink*)TTextLink::operator new(1);
+	strcpy_s(t->str, s);
+	t->pNext = pCurr->pNext;
+	t->pDown = NULL;
+	pCurr->pNext = t;
+}
+
+void TText::InsNextSection(char* s)
+{
+	TTextLink* t = (TTextLink*)(TTextLink::operator new(1));
+	strcpy_s(t->str, s);
+	t->pNext = NULL;
+	t->pDown = pCurr->pNext;
+	pCurr->pNext = t;
+}
+
+void TText::InsDownLine(char* s)
+{
+	TTextLink* t = (TTextLink*)(TTextLink::operator new(1));
+	strcpy_s(t->str, s);
+	t->pNext = pCurr->pDown;
+	t->pDown = NULL;
+	pCurr->pDown = t;
+}
+
+void TText::InsDownSection(char* s)
+{
+	TTextLink* t = (TTextLink*)(TTextLink::operator new(1));
+	strcpy_s(t->str, s);
+	t->pNext = pCurr->pDown;
+	t->pDown = NULL;
+	pCurr->pDown = t;
+}
+
+void TText::DelNextLine()
+{
+	if (pCurr->pNext)
+	{
+		TTextLink* t = pCurr->pNext;
+		pCurr->pNext = t->pNext;
+		TTextLink::operator delete(t);
+	}
+}
+
+void TText::DelDownLine()
+{
+	if (pCurr->pDown)
+	{
+		TTextLink* t = pCurr->pDown;
+		pCurr->pDown = t->pNext;
+		TTextLink::operator delete(t);
+	}
+}
+
+void TText::Print()
+{
+	level = 0;
+	PrintRec(pFirst);
+}
+
+void TText::PrintRec(TTextLink* t)
+{
+	if (t != NULL)
+	{
+		for (int i = 0; i < level; i++)
+			std::cout << ' ';
+		std::cout << t->str << '\n';
+		level++;
+		PrintRec(t->pDown);
+		level--;
+		PrintRec(t->pNext);
+	}
+}
+
+void TText::Save(const char* fn)
+{
+	std::ofstream osf;
+	osf.open(fn);
+	SaveRec(pFirst, osf);
+	osf.close();
+}
+
+void TText::SaveRec(TTextLink* t, std::ofstream& osf)
+{
+	if (t != NULL)
+	{
+		osf << t->str << std::endl;
+		if (t->pDown != NULL)
+		{
+			osf << '{' << std::endl;
+			SaveRec(t->pDown, osf);
+			osf << '}' << std::endl;
+		}
+		if (t->pNext != NULL)
+			SaveRec(t->pNext, osf);
+	}
+}
+
+void TText::Read(const char* fn)
+{
+	std::ifstream ifs(fn);
+	pFirst = ReadRec(ifs);
+	ifs.close();
+}
+
+TTextLink* TText::ReadRec(std::ifstream& ifs)
+{
+	TTextLink* pF = NULL, * pC = NULL;
+	char Buff[80];
+	while (ifs.eof() == false)
+	{
+		ifs.getline(Buff, 80, '\n');
+		if (Buff[0] == '}')
+			break;
+		else
+			if (Buff[0] == '{')
+			{
+				pC->pDown = ReadRec(ifs);
+			}
+			else
+			{
+				if (pC == NULL)
+				{
+					TTextLink* tmp = (TTextLink*)TTextLink::operator new(1);
+					strcpy_s(tmp->str, Buff);
+					tmp->pDown = NULL;
+					tmp->pNext = NULL;
+					pF = pC = tmp;
+				}
+				else
+				{
+					TTextLink* tmp = (TTextLink*)TTextLink::operator new(1);
+					strcpy_s(tmp->str, Buff);
+					tmp->pDown = NULL;
+					tmp->pNext = NULL;
+					pC->pNext = tmp;
+					pC = tmp;
+				}
+			}
+	}
+	return pF;
+}
+
+void TText::Reset()
+{
+	if (pFirst != NULL)
+	{
+		st.Clear();
+	}
+	pCurr = pFirst;
+	st.Push(pFirst);
+	if (pFirst->pNext)
+		st.Push(pFirst->pNext);
+	if (pFirst->pDown)
+		st.Push(pFirst->pDown);
+}
+
+void TText::GoNext()
+{
+	pCurr = st.Pop();
+	if (pCurr != pFirst)
+	{
+		if (pCurr->pNext)
+			st.Push(pCurr->pNext);
+		if (pCurr->pDown)
+			st.Push(pCurr->pDown);
+	}
+}
+
+bool TText::IsEnd()
+{
+	return st.Empty();
 }
